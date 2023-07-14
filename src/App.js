c9ffca62a3f2b410560a169f24e3678d5c58bd3e
Flamintouch <flamintouchservices@gmail.com>
2023-07-14 13:54:58 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -9,15 +9,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <nav>
+        <ul>
+          <li>ListItem</li>
+          <li>ListItem</li>
+          <li>ListItem</li>
+        </ul>
+      </nav>
     </div>
   );
 }
